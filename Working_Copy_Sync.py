09b9513c5cacaeff19635927f25f3a4815a9023d
Andrew <ahenry91@users.noreply.github.com>
2016-03-31 09:53:28 -0500

    Remove hard coded script name

--- Working_Copy_Sync.py
+++ Working_Copy_Sync.py
@@ -18,6 +18,7 @@ except:
 	from urllib.parse import urlencode
 
 DOCS_DIR = os.path.expanduser('~/Documents')
+WC_FILENAME = os.path.split(__file__)[-1]
 CONFIG_FILE = '.wcsync'
 
 class WorkingCopySync():
@@ -78,9 +79,9 @@ class WorkingCopySync():
 
 	def _get_repo_list(self):
 		action = 'repos'
-		fmt = 'pythonista3://{install_path}/Working_Copy_Sync.py?action=run&argv=repo_list&argv='
+		fmt = 'pythonista3://{install_path}/{wc_file}?action=run&argv=repo_list&argv='
 		payload = {
-			'x-success': fmt.format(install_path=self.install_path)
+			'x-success': fmt.format(install_path=self.install_path, wc_file=WC_FILENAME)
 		}
 		self._send_to_working_copy(action, payload)
 
@@ -93,10 +94,10 @@ class WorkingCopySync():
 			repo_name = dialogs.list_dialog(title='Select repo', items=repo_list)
 			if repo_name:
 				action = 'zip'
-				fmt = 'pythonista3://{install_path}/Working_Copy_Sync.py?action=run&argv=copy_repo&argv={repo_name}&argv='
+				fmt = 'pythonista3://{install_path}/{wc_file}?action=run&argv=copy_repo&argv={repo_name}&argv='
 				payload = {
 					'repo': repo_name,
-					'x-success': fmt.format(install_path=self.install_path, repo_name=repo_name)
+					'x-success': fmt.format(install_path=self.install_path, repo_name=repo_name, wc_file=WC_FILENAME)
 				}
 				self._send_to_working_copy(action, payload)
 
@@ -132,12 +133,12 @@ class WorkingCopySync():
 
 	def overwrite_with_wc_copy(self):
 		action = 'read'
-		fmt = 'pythonista3://{install_path}/Working_Copy_Sync.py?action=run&argv=overwrite_file&argv={path}&argv='
+		fmt = 'pythonista3://{install_path}/{wc_file}?action=run&argv=overwrite_file&argv={path}&argv='
 		payload = {
 			'repo': self.repo,
 			'path': self.path,
 			'base64': '1',
-			'x-success': fmt.format(install_path=self.install_path, path=editor.get_path())
+			'x-success': fmt.format(install_path=self.install_path, path=editor.get_path(), wc_file=WC_FILENAME)
 		}
 		self._send_to_working_copy(action, payload)
 
