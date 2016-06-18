ce67121351e3a9a3495e80fe61bf16b5e963aeab
Marco Rossi <m.rossi@outlook.com>
2016-06-18 19:23:57 +0200

    Fix for Python 3 and Pythonista 3.

--- Working_Copy_Sync.py
+++ Working_Copy_Sync.py
@@ -172,7 +172,7 @@ class WorkingCopySync():
 			console.alert('Overwriting existing directory', button1='Continue')
 			shutil.rmtree(dest)
 		zip_file_location = os.path.join(DOCS_DIR, tmp_zip_location)
-		with open(zip_file_location, 'w') as out_file:
+		with open(zip_file_location, 'wb') as out_file:
 			out_file.write(base64.b64decode(b64_contents))
 		with zipfile.ZipFile(zip_file_location) as in_file:
 			in_file.extractall(dest)
